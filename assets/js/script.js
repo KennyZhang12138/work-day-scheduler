aaa7fd23536b0a30b84c6fa318f02170bb8701b5
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-07-15 16:03:08 -0500

    add starting code and display current time zoom

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var currentDay = $("#currentDay");
+var today = dayjs();
+var hourNow = today.hour();
+
+//update today's time second to second
+function doDate() {
+  today = dayjs();
+  currentDay.text(today.format("dddd, MMMM D, YYYY h:mm:ss A"));
+  //console.log(today.hour());
+}
+setInterval(doDate, 1000);
+
+//check the timeBlock class "past", "present", and "future"
+function checkTime() {
+  var timeBlock = document.querySelectorAll(".time-block");
+
+  for (var i = 0; i < timeBlock.length; i++) {
+    var timeZoom = parseInt(timeBlock[i].getAttribute("data-hour"));
+    if (timeZoom < hourNow) {
+      timeBlock[i].setAttribute("class", "row past time-block");
+    } else if (timeZoom > hourNow) {
+      timeBlock[i].setAttribute("class", "row future time-block");
+    } else {
+      timeBlock[i].setAttribute("class", "row present time-block");
+    }
+  }
+}
+
+
+$(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+});
+
+
+checkTime();
